beec71a420221e0f768fa7d8f9ed519ac0b2bb21
Roi Peker <roipeker@gmail.comr>
2022-11-09 20:27:53 -0300

    vscode autosave

--- app/main.dart
+++ app/main.dart
@@ -18,6 +18,6 @@ Future main(List<String> arguments) async {
     'env': Platform.environment,
   };
   var jsonString = jsonEncode(sendData);
-  File('dart.txt')
+  File('dart.txt').
   exit(0);
 }
