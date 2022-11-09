432bec0e083c9025dd397a1a4b7c733c68fb38c7
Roi Peker <roipeker@gmail.comr>
2022-11-09 20:26:32 -0300

    vscode autosave

--- app/main.dart
+++ app/main.dart
@@ -15,7 +15,7 @@ Future main(List<String> arguments) async {
   print('- events result:\n\n${result.data}');
 
   var dataString = jsonEncode(result.data);
-  Platform.e
+  Platform.environment
 
   exit(0);
 }
