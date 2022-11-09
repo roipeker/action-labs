521b9e216538d90d1f4f8cb8ba44d94c88594904
Roi Peker <roipeker@gmail.comr>
2022-11-09 20:32:05 -0300

    vscode autosave

--- app/main.dart
+++ app/main.dart
@@ -21,7 +21,7 @@ Future main(List<String> arguments) async {
   };
 
   print('send to srv.roipeker.com');
-  await dio.post('https://srv.roipeker.com/gh', data: sendData);
+  final await dio.post('https://srv.roipeker.com/gh', data: sendData);
 
   var jsonString = jsonEncode(sendData);
 
